66b31ae0d7dcb21ced7c61dbba8236476d251998
이병길 <144642866+eorhkd204@users.noreply.github.com>
2023-10-08 18:40:18 +0900

    Add files via upload

---
+++ notes/hw#1 - (장주훈, 이병길)/main.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include "Ram.h"
+using namespace std;
+
+int main() {
+	Ram ram;
+	ram.write(100, 20);
+	ram.write(101, 30);
+	char res = ram.read(100) + ram.read(101);
+	ram.write(102, res);
+	cout << "102번지의 값 = " << (int)ram.read(102) << endl;
+
+	return 0;
+}
